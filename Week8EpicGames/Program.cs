fd5978929cc827d6d1ad2296907e030475b63858
Aleksandr Bychkov <aleksandr.bychkov@tktk.ee>
2023-11-30 16:03:58 +0200

    first commit

---
+++ Week8EpicGames/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+string folderPath = @"C:\Users\Aleksandr\source\repos\Week8EpicGames\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+string[] weapon = { "sword", "knife", "gun", "c4", "dark magic"};
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
